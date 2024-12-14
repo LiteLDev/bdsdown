4938676fd66ecbb5f40ae76082b695effe63d860
ShrBox <shrbox@outlook.com>
2024-12-14 22:39:14 +0800

    fix: fix download for new url

--- downloader.go
+++ downloader.go
@@ -1,56 +1,81 @@
 package main
 
 import (
-	"context"
+	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path/filepath"
 	"time"
-
-	"github.com/LiteLDev/pget"
 )
 
-func newDownloadClient(maxIdleConnsPerHost int) *http.Client {
-	tr := http.DefaultTransport.(*http.Transport).Clone()
-	tr.MaxIdleConns = 0 // no limit
-	tr.MaxIdleConnsPerHost = maxIdleConnsPerHost
-	tr.DisableCompression = true
-	return &http.Client{
-		Transport: tr,
-	}
-}
-
 func DownloadFile(url, dest string) error {
-	client := newDownloadClient(16)
-
-	target, err := pget.Check(context.Background(), &pget.CheckConfig{
-		URLs:    []string{url},
-		Timeout: 10 * time.Second,
-		Client:  client,
-	})
-	if err != nil {
-		return err
-	}
-
+	// If there's a file, don't download it twice
 	if _, err := os.Stat(dest); err == nil {
 		return nil
 	}
 
 	dir, _ := filepath.Split(dest)
 	os.MkdirAll(dir, 0755)
+	out, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
 
-	opts := []pget.DownloadOption{
-		pget.WithUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36", ""),
-		pget.WithReferer(""),
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
 	}
 
-	err = pget.Download(context.Background(), &pget.DownloadConfig{
-		Filename:      target.Filename,
-		Dirname:       dir,
-		ContentLength: target.ContentLength,
-		Procs:         config.Procs,
-		URLs:          target.URLs,
-		Client:        client,
-	}, opts...)
-	return err
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	// Get file size
+	size := resp.ContentLength
+	progress := &ProgressReader{Reader: resp.Body, Total: size}
+
+	// Enable progressbar goroutine
+	go progress.PrintProgress()
+
+	_, err = io.Copy(out, progress)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type ProgressReader struct {
+	io.Reader
+	Total   int64
+	Current int64
+}
+
+func (pr *ProgressReader) Read(p []byte) (int, error) {
+	n, err := pr.Reader.Read(p)
+	pr.Current += int64(n)
+	return n, err
+}
+
+func (pr *ProgressReader) PrintProgress() {
+	for {
+		percentage := float64(pr.Current) / float64(pr.Total) * 100
+		log.Infof("Downloading... %.2f%% complete", percentage)
+		if pr.Current >= pr.Total {
+			log.Info("Download complete")
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.23
 toolchain go1.23.4
 
 require (
-	github.com/LiteLDev/pget v0.2.5
 	github.com/PuerkitoBio/goquery v1.10.0
 	github.com/alexflint/go-arg v1.5.1
 	github.com/cheggaaa/pb/v3 v3.1.5
@@ -14,23 +13,14 @@ require (
 )
 
 require (
-	github.com/Code-Hex/updater v0.0.0-20160712085121-c3f278672520 // indirect
-	github.com/Songmu/prompter v0.5.0 // indirect
 	github.com/VividCortex/ewma v1.2.0 // indirect
 	github.com/alexflint/go-scalar v1.2.0 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
-	github.com/antonholmquist/jason v1.0.1-0.20160829104012-962e09b85496 // indirect
-	github.com/asaskevich/govalidator v0.0.0-20161001163130-7b3beb6df3c4 // indirect
 	github.com/fatih/color v1.15.0 // indirect
-	github.com/jessevdk/go-flags v0.0.0-20160903113131-4cc2832a6e6d // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/mcuadros/go-version v0.0.0-20141206211339-d52711f8d6be // indirect
-	github.com/pkg/errors v0.8.1-0.20161002052512-839d9e913e06 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	golang.org/x/net v0.29.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/term v0.24.0 // indirect
 )
